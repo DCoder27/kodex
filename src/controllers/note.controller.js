640567b71fe61bbde422a7f2dfcd86b2f837a15d
DCoder27 <priyatoshpal7@gmail.com>
2026-06-14 19:15:36 +0530

    Add PATCH /notes/:id endpoint (Update Description)

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -64,4 +64,48 @@ const getNoteByIDController = async (req, res) => {
   }
 };
 
-export { createNoteController, getNotesController, getNoteByIDController };
+const updateDescriptionController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+
+    /* -------- Validation for description ---- */
+    if (!description) {
+      return res.status(400).json({
+        message: "Description is required",
+      });
+    }
+    if (description.trim().length < 10)
+      return res.status(400).json({
+        message: "Description must be at least 10 characters long",
+      });
+
+    /* -------- Find the note by ID ---- */
+    const note = await NoteModel.findById(id);
+    if (!note)
+      return res.status(404).json({
+        message: "Note not found",
+      });
+
+    /* -------- Update the description ---- */
+    note.description = description;
+    await note.save();
+
+    /* -------- Return the updated note ---- */
+    return res.status(200).json({
+      message: "Description updated successfully",
+      Note: note,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export {
+  createNoteController,
+  getNotesController,
+  getNoteByIDController,
+  updateDescriptionController,
+};
--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { createNoteController, getNoteByIDController, getNotesController } from "../controllers/note.controller.js";
+import {
+  createNoteController,
+  getNoteByIDController,
+  getNotesController,
+  updateDescriptionController,
+} from "../controllers/note.controller.js";
 
 const router = express.Router();
 
@@ -8,21 +13,27 @@ const router = express.Router();
  * @desc Create a new note
  * @access Public
  */
-router.post('/notes', createNoteController)
+router.post("/notes", createNoteController);
 
 /**
  * @route GET /api/notes
  * @desc Get all notes
  * @access Public
  */
-router.get('/notes', getNotesController)
+router.get("/notes", getNotesController);
 
 /**
  * @route GET /api/notes/:id
  * @desc Get a note by ID
  * @access Public
  */
-router.get('/notes/:id',getNoteByIDController)
+router.get("/notes/:id", getNoteByIDController);
 
+/**
+ * @route PATCH /api/notes/:id
+ * @desc Update a note's description by ID
+ * @access Public
+ */
+router.patch("/notes/:id", updateDescriptionController);
 
 export default router;
